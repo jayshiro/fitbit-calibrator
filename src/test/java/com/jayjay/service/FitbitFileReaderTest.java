2fc8f897acfe2b78d4e4f94b4b7665fbd2384abc
Jayjay Lumanang <jayshiro@gmail.com>
2016-12-03 13:26:02 +1100

    Test that indeed the trainees are being added properly with the correct values

--- src/test/java/com/jayjay/service/FitbitFileReaderTest.java
+++ src/test/java/com/jayjay/service/FitbitFileReaderTest.java
@@ -1,7 +1,10 @@
 package com.jayjay.service;
 
 import com.jayjay.exception.*;
+import com.jayjay.model.Direction;
 import com.jayjay.model.Field;
+import com.jayjay.model.Position;
+import com.jayjay.model.Trainee;
 import com.jayjay.validation.TraineeCoordinatesValidator;
 import com.jayjay.validation.TraineeMovementsValidator;
 import com.jayjay.validation.UpperRightCoordinatesValidator;
@@ -11,6 +14,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 import static junit.framework.Assert.assertEquals;
@@ -100,5 +104,30 @@ public class FitbitFileReaderTest {
             throws InvalidRowException, IOException, InvalidFileExtensionException {
         File invalidFile = new File(getClass().getClassLoader().getResource("invalidTraineeMovements2.txt").getFile());
         fileReader.read(invalidFile.getAbsolutePath());
+    }
+
+    @Test
+    public void shouldContainTheCorrectValuesForTheField()
+            throws InvalidRowException, IOException, InvalidFileExtensionException {
+        Optional<Field> optField = fileReader.read(file.getAbsolutePath());
+        Field field = optField.get();
+        assertEquals(field.getRightX(), 5);
+        assertEquals(field.getRightY(), 5);
+
+        List<Trainee> trainees = field.getTrainees();
+        assertEquals(trainees.size(), 2);
+        Trainee trainee1 = trainees.get(0);
+        Position position1 = trainee1.getPosition();
+        assertEquals(position1.getX(), 1);
+        assertEquals(position1.getY(), 2);
+        assertEquals(position1.getDirection(), Direction.NORTH);
+        assertEquals(trainee1.getMovements(), "LMLMLMLMM");
+
+        Trainee trainee2 = trainees.get(1);
+        Position position2 = trainee2.getPosition();
+        assertEquals(position2.getX(), 3);
+        assertEquals(position2.getY(), 3);
+        assertEquals(position2.getDirection(), Direction.EAST);
+        assertEquals(trainee2.getMovements(), "MMRMMRMRRM");
     }
 }
