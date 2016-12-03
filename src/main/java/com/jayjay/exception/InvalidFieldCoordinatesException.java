290c8b74aa7c7acc2d0a1afd2f0db9e69d408055
Jayjay Lumanang <jayshiro@gmail.com>
2016-12-03 15:59:33 +1100

    Specify that the output will be a jar file; also specify the main class to run on execution

---
+++ src/main/java/com/jayjay/exception/InvalidFieldCoordinatesException.java
@@ -0,0 +1,7 @@
+package com.jayjay.exception;
+
+public class InvalidFieldCoordinatesException extends InvalidRowException{
+    public InvalidFieldCoordinatesException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/jayjay/exception/InvalidUpperRightCoordinatesException.java
+++
@@ -1,7 +0,0 @@
-package com.jayjay.exception;
-
-public class InvalidUpperRightCoordinatesException extends InvalidRowException{
-    public InvalidUpperRightCoordinatesException(String message) {
-        super(message);
-    }
-}
